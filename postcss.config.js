39b3e7e035225d114b5613b3ebf278901a07a718
orientpires <orientpires@gmail.com>
2024-08-15 22:36:15 -0300

    att

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+      'postcss-import', // Para importar arquivos CSS
+      'tailwindcss',    // Para adicionar as classes do Tailwind CSS
+      'autoprefixer',   // Para adicionar prefixos de navegador
+    ],
+  }
+  
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Ajuste conforme o caminho dos seus arquivos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
