ed0f72aedb9908d19635bbf61b5de97f99865e63
Luiz Guilherme Bandeira <arkanael@gmail.com>
2025-02-21 14:55:58 -0300

    deletando o backup

--- bkp/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-      'postcss-import', // Para importar arquivos CSS
-      'tailwindcss',    // Para adicionar as classes do Tailwind CSS
-      'autoprefixer',   // Para adicionar prefixos de navegador
-    ],
-  }
-  
